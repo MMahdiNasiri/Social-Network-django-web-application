fd722921f0505451a47041f73612bfdfd04c28e5
mahdi nasiri <mmahdi.nasiri@yahoo.com>
2020-10-28 06:48:52 -0700

    add write-tweet to homepage

--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404, JsonResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404, JsonResponse, HttpResponseRedirect
 
 from .forms import TweetForm
 from .models import Tweet
@@ -8,11 +8,16 @@ from .models import Tweet
 def home(request, *args, **kwargs):
     return render(request, 'pages/home.html', context={})
 
+
 def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
+    next_url = request.POST.get("next") or None
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
+
+        if next_url is not None:
+            return redirect(next_url)
         form = TweetForm()
     return render(request, 'components/form.html', context={"form": form})
 
